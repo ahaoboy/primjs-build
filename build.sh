ce1564d74d17a001eacb5f8fed5de22c4f6b9872
ahaoboy <504595380@qq.com>
2025-03-05 13:58:35 +0800

    init

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "not found target"
+    exit 1
+fi
+
+TARGET=$1
+
+git clone https://github.com/lynx-family/primjs
+cd primjs
+source tools/envsetup.sh
+hab sync .
+
+gn gen out/Default --args= '
+  is_debug = false'
+
+ninja -C out/Default -j32 qjs_exe
+
+
+
+mkdir ../dist
+
+ls -lh ./out/Default
+
+cp -r ./out/Default/*qjs* ../dist
+
+cd ..
+
+ls -lh dist
+
+tar -czf ./primjs-${TARGET}.tar.gz -C dist .
+ls -l ./primjs-${TARGET}.tar.gz
