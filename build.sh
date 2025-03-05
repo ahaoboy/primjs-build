ceeec61d7d98964c7187dd675ded935c0ca3699d
ahaoboy <504595380@qq.com>
2025-03-05 14:26:47 +0800

    init

--- build.sh
+++ build.sh
@@ -7,7 +7,10 @@ fi
 
 TARGET=$1
 
-git clone https://github.com/lynx-family/primjs
+# FIXME: buildroot clone error
+# git clone https://github.com/lynx-family/primjs
+git clone https://github.com/ahaoboy/primjs.git
+
 cd primjs
 source tools/envsetup.sh
 hab sync .
