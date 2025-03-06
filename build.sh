602d970712cf7159a302bcb23a8f822d11d10ead
ahaoboy <504595380@qq.com>
2025-03-06 20:04:23 +0800

    add args for mac-arm

--- build.sh
+++ build.sh
@@ -16,9 +16,31 @@ hab sync .
 ARCH=$(uname -m)
 
 if [[ "$ARCH" == "arm"* || "$ARCH" == "aarch64" ]]; then
-    gn gen out/Default --args="target_cpu=\"arm64\" enable_primjs_snapshot=true enable_compatible_mm=true enable_tracing_gc=true"
+    if [[ "$(uname)" == "Darwin" ]]; then
+      gn gen out/Default --args='
+        enable_quickjs_debugger=false
+        enable_tracing_gc = true
+        enable_compatbile_mm = true
+        enable_primjs_snapshot = true
+        target_cpu = "arm64"
+        target_os = "mac"
+        is_debug=false
+        enable_optimize_with_O2=true
+      '
+    else
+      gn gen out/Default --args='
+        target_cpu="arm64"
+        enable_primjs_snapshot = true
+        enable_compatible_mm = true
+        enable_tracing_gc = true
+      '
+    fi
 else
-    gn gen out/Default --args="enable_primjs_snapshot=true enable_compatible_mm=true enable_tracing_gc=true"
+    gn gen out/Default --args='
+        enable_primjs_snapshot=true
+        enable_compatible_mm=true
+        enable_tracing_gc=true
+    '
 fi
 
 ninja -C out/Default -j32 qjs_exe
